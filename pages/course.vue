d9d725cc465c55a6afc8b73560a793d9c15a4d42
MasihTak <masihtak@outlook.com>
2022-10-31 06:53:42 +0100

    style: UI improvements

--- pages/course.vue
+++ pages/course.vue
@@ -5,11 +5,11 @@
                 Course:
                 <strong>Mastering Nuxt 3</strong>
             </h1>
-            <div class="row justify-content-center gap-3 h-100">
+            <div class="row course-container justify-content-center gap-3">
                 <aside class="col-2 bg-white rounded-2 shadow-sm p-4">
                     <h3 class="fs-5"> Chapters</h3>
                 </aside>
-                <main class="col-6 bg-white rounded-2 shadow-sm p-4">
+                <main class="col-md-6 col-lg-7 bg-white rounded-2 shadow-sm p-4">
                     <NuxtPage/>
                 </main>
             </div>
@@ -26,5 +26,9 @@ export default {
 <style scoped lang="scss">
 .course {
   background-color: $gray-100;
+}
+
+.course-container {
+  height: calc(100vh - 200px);
 }
 </style>
