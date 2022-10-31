88778f42a351d182c57f4b6da489aa77cc0798cc
MasihTak <masihtak@outlook.com>
2022-10-31 07:52:12 +0100

    feat: add video player

--- pages/course/chapter/[chapterSlug]/lesson/[lessonslug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonslug].vue
@@ -1,8 +1,14 @@
 <template>
-  <h2>Lesson</h2>
-  <p>This is a lesson</p>
-  <p>{{ chapter.title }}</p>
-  <p>{{ lesson.title }}</p>
+  <p class="text-uppercase fw-bold text-secondary mb-0">lesson {{chapter.number}} - {{lesson.number}}</p>
+  <h2 class="mt-0 fs-3">{{ lesson.title }}</h2>
+  <a v-if="lesson.sourceUrl"
+     class="text-muted d-inline-block pb-4"
+     :href="lesson.sourceUrl">Download Source Code</a>
+    <a v-if="lesson.downloadUrl"
+       class="text-muted d-inline-block pb-4"
+       :href="lesson.downloadUrl">Download Video</a>
+    <VideoPlayer class="d-block pb-4" v-if="lesson.videoId" :videoId="lesson.videoId" />
+  <p>{{ lesson.text }}</p>
 </template>
 
 <script setup>
