3f40b1eaf5e7372a9bf0cd3675d2a28be382b468
MasihTak <masihtak@outlook.com>
2023-02-25 09:07:55 +0100

    refactor: links to nuxt-link

--- pages/course/chapter/[chapterSlug]/lesson/[lessonslug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonslug].vue
@@ -1,12 +1,12 @@
 <template>
   <p class="text-uppercase fw-bold text-secondary mb-0">lesson {{chapter.number}} - {{lesson.number}}</p>
   <h2 class="mt-0 fs-3">{{ lesson.title }}</h2>
-  <a v-if="lesson.sourceUrl"
+  <NuxtLink v-if="lesson.sourceUrl"
      class="text-muted d-inline-block pb-4"
-     :href="lesson.sourceUrl">Download Source Code</a>
-    <a v-if="lesson.downloadUrl"
+     :to="lesson.sourceUrl">Download Source Code</NuxtLink>
+    <NuxtLink v-if="lesson.downloadUrl"
        class="text-muted d-inline-block pb-4"
-       :href="lesson.downloadUrl">Download Video</a>
+       :to="lesson.downloadUrl">Download Video</NuxtLink>
     <VideoPlayer class="d-block pb-4" v-if="lesson.videoId" :videoId="lesson.videoId" />
   <p>{{ lesson.text }}</p>
 </template>
