561a29dbb83d3d1cd159a636f148615c18d1eedc
MasihTak <masihtak@outlook.com>
2023-02-25 08:39:56 +0100

    feat: load course data dynamically

--- pages/course.vue
+++ pages/course.vue
@@ -7,7 +7,28 @@
             </h1>
             <div class="row course-container justify-content-center gap-3">
                 <aside class="col-2 bg-white rounded-2 shadow-sm p-4">
-                    <h3 class="fs-5"> Chapters</h3>
+                    <h3 class="fs-5 fw-bold mb-4"> Chapters</h3>
+                    <div class="d-flex flex-column mb-2"
+                        v-for="chapter in chapters"
+                         :key="chapter.slug"
+                    >
+                        <h4 class="fs-6 fw-bold"> {{ chapter.title}} </h4>
+                        <NuxtLink 
+                            v-for="(lesson, index) in chapter.lessons"
+                            class="d-flex flex-row ms-1 text-decoration-none mb-2"
+                           :key="lesson.slug"
+                           :to="lesson.path"
+                            :class="{
+                               'text-success':
+                               lesson.path === $route.fullpath,
+                               'text-reset':
+                               lesson.path !== $route.fullpath
+                            }"
+                        >
+                            <span class="text-muted pe-1">{{ index + 1}}.</span>
+                            <span>{{ lesson.title }}</span>
+                        </NuxtLink>
+                    </div>
                 </aside>
                 <main class="col-md-6 col-lg-7 bg-white rounded-2 shadow-sm p-4">
                     <NuxtPage/>
@@ -17,10 +38,8 @@
     </div>
 </template>
 
-<script>
-export default {
-    name: "course"
-}
+<script setup>
+const { chapters } = useCourse();
 </script>
 
 <style scoped lang="scss">
