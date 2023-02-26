22af0a0fa850b8a6bb0784a8d3dddb647a72ae87
MasihTak <masihtak@outlook.com>
2023-02-26 22:53:14 +0100

    refactor: sidebar to it's own component

---
+++ components/AppSidebar.vue
@@ -0,0 +1,30 @@
+<template>
+	<aside class="col-2 bg-white rounded-2 shadow-sm p-4">
+		<h3 class="fs-5 fw-bold mb-4">Chapters</h3>
+		<div class="d-flex flex-column mb-2"
+		     v-for="chapter in chapters"
+		     :key="chapter.slug"
+		>
+			<h4 class="fs-6 fw-bold">{{chapter.title}} </h4>
+			<NuxtLink
+					v-for="(lesson, index) in chapter.lessons"
+					class="d-flex flex-row ms-1 text-decoration-none mb-2"
+					:key="lesson.slug"
+					:to="lesson.path"
+					:class="{
+              'text-primary':
+                lesson.path === $route.fullPath,
+              'text-muted':
+                lesson.path !== $route.fullPath,
+            }"
+			>
+				<span class="text-muted pe-1">{{index + 1}}.</span>
+				<span>{{lesson.title}}</span>
+			</NuxtLink>
+		</div>
+	</aside>
+</template>
+
+<script setup>
+const {chapters} = useCourse();
+</script>
--- pages/course.vue
+++ pages/course.vue
@@ -6,33 +6,7 @@
 		        3</strong>
 	</h1>
 	<div class="row course-container justify-content-center gap-3 pb-5">
-		<aside class="col-2 bg-white rounded-2 shadow-sm p-4">
-			<h3 class="fs-5 fw-bold mb-4">
-				Chapters</h3>
-			<div class="d-flex flex-column mb-2"
-			     v-for="chapter in chapters"
-			     :key="chapter.slug"
-			>
-				<h4 class="fs-6 fw-bold">
-					{{chapter.title}} </h4>
-				<NuxtLink
-						v-for="(lesson, index) in chapter.lessons"
-						class="d-flex flex-row ms-1 text-decoration-none mb-2"
-						:key="lesson.slug"
-						:to="lesson.path"
-						:class="{
-                               'text-success':
-                               lesson.path === $route.fullpath,
-                               'text-reset':
-                               lesson.path !== $route.fullpath
-                            }"
-				>
-					<span class="text-muted pe-1">{{index + 1}}.</span>
-					<span>{{lesson.title
-					      }}</span>
-				</NuxtLink>
-			</div>
-		</aside>
+		<AppSidebar />
 		<main class="col-md-6 col-lg-7 bg-white rounded-2 shadow-sm p-4">
 			<NuxtPage/>
 		</main>
@@ -40,11 +14,9 @@
 </template>
 
 <script setup>
-const {chapters} = useCourse();
+
 </script>
 
 <style scoped lang="scss">
-.router-link-active {
-  color : $blue-500 !important;
-  }
+
 </style>
