66b8cebd9e94a0d3de137634efecd5e03e27db6b
MasihTak <masihtak@outlook.com>
2023-03-02 22:49:23 +0100

    style: minor UI improvements - sidebar

--- components/AppSidebar.vue
+++ components/AppSidebar.vue
@@ -1,14 +1,14 @@
 <template>
-	<aside class="col-2 bg-white rounded-2 shadow-sm p-4">
+	<aside class="col-3 bg-white rounded-2 shadow-sm p-4">
 		<h3 class="fs-5 fw-bold mb-4">Chapters</h3>
 		<div class="d-flex flex-column mb-2"
 		     v-for="chapter in chapters"
 		     :key="chapter.slug"
 		>
-			<h4 class="fs-6 fw-bold">{{chapter.title}} </h4>
+			<h4 class="fs-6 pb-2">{{chapter.title}} </h4>
 			<NuxtLink
 					v-for="(lesson, index) in chapter.lessons"
-					class="d-flex flex-row ms-1 text-decoration-none mb-2"
+					class="d-flex flex-row ms-1 text-decoration-none mb-3"
 					:key="lesson.slug"
 					:to="lesson.path"
 					:class="{
@@ -28,3 +28,9 @@
 <script setup>
 const {chapters} = useCourse();
 </script>
+
+<style scoped>
+aside {
+	max-width: 30ch;
+}
+</style>
