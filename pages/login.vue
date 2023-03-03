2864e6ad6c0bea30485b015746c4b8bd343668d5
MasihTak <masihtak@outlook.com>
2023-03-03 23:36:17 +0100

    feat: add a simple login page

---
+++ pages/login.vue
@@ -0,0 +1,10 @@
+<template>
+ <div>
+	 <h1 class="fw-bold">Login in to {{title}}</h1>
+	 <button class="btn btn-primary">Login in with Github</button>
+ </div>
+</template>
+
+<script setup>
+const {title} = useCourse()
+</script>
