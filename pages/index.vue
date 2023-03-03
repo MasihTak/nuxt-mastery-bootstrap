5326ce5fc92586163b3129f8f76346bdd43bf4e1
MasihTak <masihtak@outlook.com>
2023-03-03 23:28:34 +0100

    refactor: home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,17 @@
 <template>
-    <p>Hello, world!</p>
+	<div class="pt-5 text-center">
+		<NuxtLink :to="firstLesson.path"
+		          class="text-black fw-bold"
+		>
+			Go to the first Lesson
+		</NuxtLink>
+	</div>
+
 </template>
+
+<script setup>
+const firstLesson = useFirstLesson();
+</script>
 
 
 <style lang="scss">
