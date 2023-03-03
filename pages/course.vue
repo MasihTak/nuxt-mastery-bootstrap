5cfe5b24ad2846dc36644cf3d2ea91312a7fb45f
MasihTak <masihtak@outlook.com>
2023-03-03 23:34:40 +0100

    refactor: course page

--- pages/course.vue
+++ pages/course.vue
@@ -1,38 +1,36 @@
 <template>
-	<h1 class="text-center py-5">
-		Course:
-		<strong>Mastering
-		        Nuxt
-		        3</strong>
-	</h1>
-	<div class="row course-container justify-content-center gap-3 pb-5">
-		<AppSidebar />
-		<main class="col-md-6 col-lg-7 bg-white rounded-2 shadow-sm p-4">
-			<NuxtErrorBoundary>
-				<NuxtPage/>
-				<template #error="{error}">
-					<p>
-						Oh no, something went wrong with the lesson!
-						<code>{{error}}</code>
-					</p>
-					<button type="button"
-					        class="btn btn-secondary"
-					        @click="resetError(error)"
-					>Reset</button>
-				</template>
-			</NuxtErrorBoundary>
-		</main>
+	<div>
+		<h1 class="text-center py-5">
+			<strong>{{title}}</strong>
+		</h1>
+		<div class="row course-container justify-content-center gap-3 pb-5">
+			<AppSidebar/>
+			<main class="col-md-6 col-lg-7 bg-white rounded-2 shadow-sm p-4">
+				<NuxtErrorBoundary>
+					<NuxtPage/>
+					<template #error="{error}">
+						<p>Oh no, something went wrong with the lesson!
+							<code>{{error}}</code>
+						</p>
+						<button type="button"
+						        class="btn btn-secondary"
+						        @click="resetError(error)"
+						>
+							Reset
+						</button>
+					</template>
+				</NuxtErrorBoundary>
+			</main>
+		</div>
 	</div>
 </template>
 
 <script setup>
+const {title} = useCourse()
+
 const resetError = async (error) => {
 	// We should navigate first, and then set the error value to null
 	await navigateTo('/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3')
 	error.value = null;
 }
 </script>
-
-<style scoped lang="scss">
-
-</style>
