5531e022c54ec7955e20e191c888da17e7b78956
MasihTak <masihtak@outlook.com>
2023-12-29 19:40:54 +0100

    feat: configure supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,14 @@
 export default defineNuxtConfig({
     telemetry: false,
-
     modules: ['@vueuse/nuxt', '@nuxtjs/supabase'],
+
+    supabase: {
+        redirectOptions: {
+            login: '/login',
+            callback: '/confirm',
+            exclude: ['/*'], // We implement our own auth behaviour in the auth middleware
+        },
+    },
 
     vite: {
         css: {
