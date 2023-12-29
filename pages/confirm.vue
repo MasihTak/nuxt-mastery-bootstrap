fbc1b9c43f350615920dff326df30b9c1643e122
MasihTak <masihtak@outlook.com>
2023-12-29 19:42:05 +0100

    feat: add confirm page

---
+++ pages/confirm.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const user = useSupabaseUser()
+watch(user, () => {
+  if (user.value) {
+    return navigateTo('/')
+  }
+}, { immediate: true })
+</script>
+
+<template>
+  <div>
+    <p>Waiting for login...</p>
+  </div>
+</template>
