1affa9bc06b32a6a98b31bebc1ce9eeb87ccc38b
MasihTak <masihtak@outlook.com>
2023-12-29 23:14:51 +0100

    feat: logging in with Github

--- pages/login.vue
+++ pages/login.vue
@@ -1,10 +1,25 @@
 <template>
- <div>
+ <section class="mt-5">
 	 <h1 class="fw-bold">Login in to {{title}}</h1>
-	 <button class="btn btn-primary">Login in with Github</button>
- </div>
+	 <button class="btn btn-primary mt-3"
+           @click.prevent="login"
+   >
+     Login in with Github
+   </button>
+ </section>
 </template>
 
 <script setup>
-const {title} = useCourse()
+const {title} = useCourse();
+const supabase = useSupabaseClient();
+
+const login = async () => {
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: 'github'
+  });
+
+  if (error) {
+    console.error(error);
+  }
+}
 </script>
